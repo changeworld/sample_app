318c68fb2b9bea0e902a03f75a9a24d6926b50cc
changeworld <changesworlds@gmail.com>
2014-09-13 23:48:14 +0900

    Finish updating users exercises 3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,12 +9,16 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
 
     describe "with invalid information" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
