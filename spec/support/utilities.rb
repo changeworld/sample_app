9f5a6e1b953bb6edb1c483da16a86e3a0dff6f4a
changeworld <changesworlds@gmail.com>
2014-09-24 00:03:10 +0900

    Use only ascii symbols in comments close #45

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,7 +9,7 @@ end
 
 def sign_in(user, options = {})
   if options[:no_capybara]
-    # Capybaraを使用していない場合にもサインインする。
+    # Sign in even if not using the Capybara.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
